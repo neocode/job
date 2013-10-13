428c5f929c8101c87d8628c42652c50d936d787b
alex <sheremet-a@mail.ru>
2013-10-13 13:10:24 +0300

    upload 3.

--- dynamic_model/dynamic_model/admin.py
+++ dynamic_model/dynamic_model/admin.py
@@ -2,6 +2,6 @@ __author__ = 'alex'
 
 from django.contrib import admin
 
-from dynamic_model.models import model_list
-for model in model_list:
+from dynamic_model.models import supermodel_list
+for model in supermodel_list:
     admin.site.register(model)
--- dynamic_model/dynamic_model/models.py
+++ dynamic_model/dynamic_model/models.py
@@ -4,6 +4,7 @@ from dynamic_model.yamls import doc
 
 print doc
 
+
 def install(model):
     from django.core.management import sql, color
     from django.db import connection
@@ -12,12 +13,14 @@ def install(model):
 
     cursor = connection.cursor()
     statements = sql.sql_create(model, style, connection)
+    print statements
     for sql in statements:
         cursor.execute(sql)
 
 
 names = ('first', 'second', 'third', 'fourth')
 model_list = []
+supermodel_list = []
 
 for i in names:
     dct = {
@@ -28,6 +31,21 @@ for i in names:
     model = type(i, (models.Model,), dct)
     model_list.append(model)
     install(model)
+
+print model_list
+
+dct = {
+    "__module__": "dynamic_model.models",
+    "title": models.CharField(max_length=255),
+    "first": models.ForeignKey(model_list[0]),
+    "second": models.ForeignKey(model_list[1]),
+    "third": models.ForeignKey(model_list[2]),
+    "fourth": models.ForeignKey(model_list[3]),
+}
+supermodel = type('This_new', (models.Model,), dct)
+supermodel_list.append(supermodel)
+install(supermodel)
+
 
 
 '''
